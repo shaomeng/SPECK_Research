03e478ef374f6c1f26b042b280af3ebd3b92dfb3
Samuel Li <shaomenglee@gmail.com>
2017-01-18 21:35:09 -0800

    add raw2imagecube utility

--- utilities/bin/setenv.sh
+++ utilities/bin/setenv.sh
@@ -10,12 +10,15 @@ if [[ "$ARCH" == 'Darwin' ]]; then
   QCC_DIR='/Users/shaomeng/Install/QccPack-0.61'
   export DYLD_LIBRARY_PATH=${QCC_DIR}/lib:$(echo $DYLD_LIBRARY_PATH)
   export PATH=${QCC_DIR}/bin:$(echo $PATH)
+  export MANPATH=${QCC_DIR}/man:$(echo $MANPATH)
 elif [[ $USER == 'shaomeng' ]]; then
   QCC_DIR='/glade/u/home/shaomeng/lib_gnu/QccPack-0.61'
   export LD_LIBRARY_PATH=${QCC_DIR}/lib:$(echo $LD_LIBRARY_PATH)
   export PATH=${QCC_DIR}/bin:$(echo $PATH)
+  export MANPATH=${QCC_DIR}/man:$(echo $MANPATH)
 else              # Alaska
   QCC_DIR='/home/users/samuelli/Install/QccPack-0.61'
   export LD_LIBRARY_PATH=${QCC_DIR}/lib:$(echo $LD_LIBRARY_PATH)
   export PATH=${QCC_DIR}/bin:$(echo $PATH)
+  export MANPATH=${QCC_DIR}/man:$(echo $MANPATH)
 fi
