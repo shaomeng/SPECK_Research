64939fa3f352b75c0c3d54eb25a4c30bc15c629a
Samuel Li <shaomenglee@gmail.com>
2017-01-20 19:58:41 -0700

    finish imagecubeDwtCompression utility

--- utilities/imagecubeDwtCompression.c
+++ utilities/imagecubeDwtCompression.c
@@ -1,6 +1,7 @@
 /*
  * This program takes in an image cube, applies wavelet transform,
- * thresholds the coefficients, and then applies inverse wavelet transform.
+ * thresholds the coefficients, applies inverse wavelet transform,
+ * and then writes the result to disk.
  */
 
 #include "stdio.h"
@@ -8,84 +9,103 @@
 #include "assert.h"
 #include "libQccPack.h"
 
+int compare_float (const void *a, const void *b)
+{
+  // result in decending order
+  const float *da = (const float *) a;
+  const float *db = (const float *) b;
+
+  return (*da < *db) - (*da > *db);
+}
 
 int main( int argc, char* argv[] )
 {
-	if( argc != 2 )
+	if( argc != 4 )
 	{
-		perror("Usage: ./raw2pyramid3d input_pyramid3d_file_(spbt)\n ");
-		exit(1);
+		perror("Usage: ./imagecubeDwtCompression input_icb output_icb cratio\n ");
+		exit(0);
 	}
 
-	QccWAVSubbandPyramid3D subband_pyramid;
-  QccWAVSubbandPyramid3DInitialize( &subband_pyramid );
+  int cratio = atoi( argv[3] );
 
-  /* read input pyramid3d */
-  QccConvertToQccString( subband_pyramid.filename, argv[1] );
-  if( QccWAVSubbandPyramid3DRead( &subband_pyramid ) != 0 )
-    perror("QccWAVSubbandPyramid3DRead() failed!\n");
-  
+  /* read input */
+  QccIMGImageCube imagecube;
+  QccIMGImageCubeInitialize( &imagecube );
+  QccConvertToQccString( imagecube.filename, argv[1] );
+  QccIMGImageCubeRead( &imagecube );
+
+  /* create a QccWAVSubbandPyramid3D based on the input */
+	QccWAVSubbandPyramid3D pyramid;
+  QccWAVSubbandPyramid3DInitialize( &pyramid );
+  pyramid.spatial_num_levels = 0;
+  pyramid.temporal_num_levels = 0;
+  pyramid.num_frames = imagecube.num_frames;
+  pyramid.num_rows = imagecube.num_rows;
+  pyramid.num_cols = imagecube.num_cols;
+  QccWAVSubbandPyramid3DAlloc( &pyramid );
+  QccVolumeCopy(  pyramid.volume, 
+                  imagecube.volume,
+                  pyramid.num_frames,
+                  pyramid.num_rows,
+                  pyramid.num_cols );
+
   /* prepare wavelets */
   QccWAVWavelet Wavelet;
-  // QccString WaveletFilename = QCCWAVWAVELET_DEFAULT_WAVELET;  /* CDF9/7 lifting scheme */
-  QccString WaveletFilename = "CohenDaubechiesFeauveau.9-7.fbk";
+  QccString WaveletFilename = QCCWAVWAVELET_DEFAULT_WAVELET;  /* CDF9/7 lifting */
+  // QccString WaveletFilename = "CohenDaubechiesFeauveau.9-7.fbk";
   QccString Boundary = "symmetric";
   if( QccWAVWaveletInitialize(&Wavelet) != 0 )
     perror("QccWAVWaveletInitialize() failed!\n");
   if( QccWAVWaveletCreate(&Wavelet, WaveletFilename, Boundary) != 0 )
     perror("QccWAVWaveletCreate() failed!\n");
-  int TransformType = QCCWAVSUBBANDPYRAMID3D_PACKET;  /* different levels in XY and Z */
-  int temporal_num_levels = 2;
-  int spatial_num_levels = 4;
+  int TransformType = QCCWAVSUBBANDPYRAMID3D_DYADIC;  /* same levels */
+  // int TransformType = QCCWAVSUBBANDPYRAMID3D_PACKET;  /* different levels */
+  int temporal_num_levels = 5;
+  int spatial_num_levels  = 5;
 
-  /* make a copy of input signal */
-	QccWAVSubbandPyramid3D subband_pyramid_test;
-  QccWAVSubbandPyramid3DInitialize( &subband_pyramid_test );
-  if( QccWAVSubbandPyramid3DCopy( &subband_pyramid_test, &subband_pyramid ) != 0 )
-    perror("QccWAVSubbandPyramid3DCopy() failed!\n");
-
-  /* DWT and IDWT test */
-  if( QccWAVSubbandPyramid3DDWT( &subband_pyramid_test, TransformType, temporal_num_levels,
-                                  spatial_num_levels, &Wavelet ) != 0 )
+  /* DWT */
+  if( QccWAVSubbandPyramid3DDWT( &pyramid, 
+                                  TransformType, 
+                                  temporal_num_levels,
+                                  spatial_num_levels, 
+                                  &Wavelet ) != 0 )
     perror("QccWAVSubbandPyramid3DDWT() failed!\n");
-  if( QccWAVSubbandPyramid3DInverseDWT( &subband_pyramid_test, &Wavelet ) != 0 )
+
+  /* thresholding coeffs */
+  if( cratio > 1 )
+  {
+    long nVals = pyramid.num_frames * pyramid.num_rows * pyramid.num_cols; 
+    float* buf = (float*) malloc( nVals * 4 );
+    long counter = 0;
+    for( long frame = 0; frame < pyramid.num_frames; frame++ )
+    for( long row   = 0; row   < pyramid.num_rows;   frame++ )
+    for( long col   = 0; col   < pyramid.num_cols;   frame++ )
+      buf[ counter++ ] = fabs( pyramid.volume[frame][row][col] );
+    qsort( buf, nVals, sizeof(float), compare_float );
+    float threshold = buf[ nVals / cratio ];
+    free( buf );
+    for( long frame = 0; frame < pyramid.num_frames; frame++ )
+    for( long row   = 0; row   < pyramid.num_rows;   frame++ )
+    for( long col   = 0; col   < pyramid.num_cols;   frame++ )
+      if( fabs(pyramid.volume[frame][row][col]) < threshold )
+        pyramid.volume[frame][row][col] = 0.0;
+  }
+      
+  /* IDWT */
+  if( QccWAVSubbandPyramid3DInverseDWT( &pyramid, &Wavelet ) != 0 )
     perror("QccWAVSubbandPyramid3DInverseDWT() failed!\n");
 
-  /* debug info
-   *
-  perror("Printing original data:\n");
-  QccWAVSubbandPyramid3DPrint( &subband_pyramid );
-  perror("\nPrinting transformed data:\n");
-  QccWAVSubbandPyramid3DPrint( &subband_pyramid_test );
-   */
+  /* Copy back to imagecube */
+  QccVolumeCopy(  imagecube.volume,
+                  pyramid.volume,
+                  pyramid.num_frames,
+                  pyramid.num_rows,
+                  pyramid.num_cols );
 
-  /* compare results */
-  long int nVals  = subband_pyramid.num_frames * subband_pyramid.num_rows * subband_pyramid.num_cols;
-  float* orig_buf = (float*) malloc( sizeof(float) * nVals );
-  float* test_buf = (float*) malloc( sizeof(float) * nVals );
-  long frame, row, col;
-  long counter = 0;
-  for(    frame = 0; frame < subband_pyramid.num_frames; frame++ )
-    for(    row = 0; row   < subband_pyramid.num_rows;   row++ )
-      for(  col = 0; col   < subband_pyramid.num_cols;   col++ )
-      {
-        orig_buf[ counter ] = subband_pyramid.volume[frame][row][col];
-        test_buf[ counter ] = subband_pyramid_test.volume[frame][row][col];
-        counter++;
-      } 
+  /* write out imagecube to a new name */
+  QccConvertToQccString( imagecube.filename, argv[2] );
+  QccIMGImageCubeWrite( &imagecube );
 
-  double rmse             = GetRMSE( orig_buf, test_buf, nVals );
-  float  maxError         = GetMaxError( orig_buf, test_buf, nVals );
-  float  maxRelativeError = GetMaxRelativeError( test_buf, orig_buf, nVals );
-  printf( "Test results:\n" );
-  printf( "  RMSE               = %.10e \n", rmse );
-  printf( "  maxError           = %.10e \n", maxError );
-  printf( "  maxRelativeError   = %.10e \n", maxRelativeError );
-
-  /* clean up */
-  free( test_buf );
-  free( orig_buf );
-  QccWAVSubbandPyramid3DFree( &subband_pyramid_test );
-  QccWAVWaveletFree(&Wavelet);
-  QccWAVSubbandPyramid3DFree( &subband_pyramid );
+  QccWAVSubbandPyramid3DFree( &pyramid );
+  QccIMGImageCubeFree( &imagecube );
 }
