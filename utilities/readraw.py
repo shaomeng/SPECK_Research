4c3fd3a13ef4df0543da09a8ccf2da95ca845d48
Samuel Li <shaomenglee@gmail.com>
2017-01-23 19:26:07 -0700

    update use log scale; use the same bins

--- utilities/readraw.py
+++ utilities/readraw.py
@@ -7,28 +7,30 @@ import numpy as np
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 
-num_bins = 200
+num_bins = 300
 
 f = open("/flash_buffer/Sam/wx.025.raw", "r" )
 a = np.fromfile( f, dtype=np.float32 )
+a = np.log(a)
+truemin = a.min()
+truemax = a.max()
 print ("length of input: " + str(len(a)) )
-y, binEdges = np.histogram(a, num_bins, normed=1)
-binCenters  = 0.5*(binEdges[1:]+binEdges[:-1])
-plt.plot( binCenters, y, 'r-', label="ground truth" )
+y, binEdges = np.histogram(a, range=(truemin, truemax), num_bins, normed=1)
+plt.plot( binEdges, y, 'r-', label="ground truth" )
 
 f = open("/flash_buffer/Sam/speck_64to1/wx.025.raw.64to1", "r" )
 a = np.fromfile( f, dtype=np.float32 )
+a = np.log(a)
 print ("length of input: " + str(len(a)) )
-y, binEdges = np.histogram(a, num_bins, normed=1)
-binCenters  = 0.5*(binEdges[1:]+binEdges[:-1])
-plt.plot( binCenters, y, 'g--', label="64:1 SPECK" )
+y, binEdges = np.histogram(a, range=(truemin, truemax), num_bins, normed=1)
+plt.plot( binEdges, y, 'g--', label="64:1 SPECK" )
 
 f = open("/flash_buffer/Sam/vdf/wx.025.raw.64to1", "r" )
 a = np.fromfile( f, dtype=np.float32 )
+a = np.log(a)
 print ("length of input: " + str(len(a)) )
-y, binEdges = np.histogram(a, num_bins, normed=1)
-binCenters  = 0.5*(binEdges[1:]+binEdges[:-1])
-plt.plot( binCenters, y, 'b--', label="64:1 VAPOR" )
+y, binEdges = np.histogram(a, range=(truemin, truemax), num_bins, normed=1)
+plt.plot( binEdges, y, 'b--', label="64:1 VAPOR" )
 
 plt.xlabel('vx')
 plt.ylabel('Probability')
