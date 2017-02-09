be8ee018f45f9f90de1a1cea849f89563960db57
Samuel Li <shaomenglee@gmail.com>
2017-02-09 11:47:08 -0700

    calculate moments up to the 8th

--- scripts/moments.py
+++ scripts/moments.py
@@ -1,4 +1,6 @@
-#!/opt/apps/gcc-4.8/python-3.5.3/bin/python3
+#!/glade/u/home/shaomeng/lib_gnu/Python-3.5.3/bin/python3
+
+###  !/opt/apps/gcc-4.8/python-3.5.3/bin/python3
 
 import sys
 from   decimal import Decimal
@@ -17,8 +19,16 @@ mom_1st = stats.moment(a, moment=1, axis=None )
 mom_2nd = stats.moment(a, moment=2, axis=None )
 mom_3rd = stats.moment(a, moment=3, axis=None )
 mom_4th = stats.moment(a, moment=4, axis=None )
-print ("Moments of the input (from 1st to 4th):" )
+mom_5th = stats.moment(a, moment=5, axis=None )
+mom_6th = stats.moment(a, moment=6, axis=None )
+mom_7th = stats.moment(a, moment=7, axis=None )
+mom_8th = stats.moment(a, moment=8, axis=None )
+print ("Moments of the input (from 1st to 8th):" )
 print ("  %.4e" % mom_1st )
 print ("  %.4e" % mom_2nd )
 print ("  %.4e" % mom_3rd )
 print ("  %.4e" % mom_4th )
+print ("  %.4e" % mom_5th )
+print ("  %.4e" % mom_6th )
+print ("  %.4e" % mom_7th )
+print ("  %.4e" % mom_8th )
