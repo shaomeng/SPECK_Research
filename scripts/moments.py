c03a3d461d004c094df0efe357df390e156aab52
Samuel Li <shaomenglee@gmail.com>
2017-02-16 12:14:14 -0700

    udpate on moments calculate script

--- scripts/moments.py
+++ scripts/moments.py
@@ -6,29 +6,80 @@ import sys
 from   decimal import Decimal
 import numpy as np
 from   scipy import stats
+import matplotlib.pyplot as plt
 
-if len(sys.argv) != 2:
-	print ("Please provide a file to process")
+if len(sys.argv) != 5:
+	print ("Please provide 4 files to process")
 	sys.exit(1)
 
-print ("Input file: " + sys.argv[1] )
-f = open(sys.argv[1], "r" )
-a = np.fromfile( f, dtype=np.float32 )
-print ("Length of input: " + str(len(a)) )
-mom_1st = stats.moment(a, moment=1, axis=None )
-mom_2nd = stats.moment(a, moment=2, axis=None )
-mom_3rd = stats.moment(a, moment=3, axis=None )
-mom_4th = stats.moment(a, moment=4, axis=None )
-mom_5th = stats.moment(a, moment=5, axis=None )
-mom_6th = stats.moment(a, moment=6, axis=None )
-mom_7th = stats.moment(a, moment=7, axis=None )
-mom_8th = stats.moment(a, moment=8, axis=None )
-print ("Moments of the input (from 1st to 8th):" )
-print ("  %.4e" % mom_1st )
-print ("  %.4e" % mom_2nd )
-print ("  %.4e" % mom_3rd )
-print ("  %.4e" % mom_4th )
-print ("  %.4e" % mom_5th )
-print ("  %.4e" % mom_6th )
-print ("  %.4e" % mom_7th )
-print ("  %.4e" % mom_8th )
+num_moments = 12
+
+f1 = open(sys.argv[1], "r" )
+a1 = np.fromfile( f1, dtype=np.float32 )
+b1 = a1.astype(np.float64)
+moments1 = [0] * num_moments
+print ("Ground truth moments: 1st to 12th")
+for i in range(num_moments):
+  moments1[i] = stats.moment(b1, moment=i+1, axis=None )
+  print ("  %.4e" % moments1[i] )
+ 
+f2 = open(sys.argv[2], "r" )
+a2 = np.fromfile( f2, dtype=np.float32 )
+b2 = a2.astype(np.float64)
+moments2 = [0] * num_moments
+print ("16:1 moments: 1st to 12th")
+for i in range(num_moments):
+  moments2[i] = stats.moment(b2, moment=i+1, axis=None )
+  print ("  %.4e" % moments2[i] )
+diff2 = [0.0] * num_moments
+for i in range(num_moments):
+	if moments1[i] != 0.0:
+		diff2[i] = (moments2[i]-moments1[i]) / moments1[i]
+	else:
+		diff2[i] = 0.0
+ 
+f3 = open(sys.argv[3], "r" )
+a3 = np.fromfile( f3, dtype=np.float32 )
+b3 = a3.astype(np.float64)
+moments3 = [0] * num_moments
+print ("32:1 moments: 1st to 12th")
+for i in range(num_moments):
+  moments3[i] = stats.moment(b3, moment=i+1, axis=None )
+  print ("  %.4e" % moments3[i] )
+diff3 = [0.0] * num_moments
+for i in range(num_moments):
+	if moments1[i] != 0.0:
+		diff3[i] = (moments3[i]-moments1[i]) / moments1[i]
+	else:
+		diff3[i] = 0.0
+ 
+f4 = open(sys.argv[4], "r" )
+a4 = np.fromfile( f4, dtype=np.float32 )
+b4 = a4.astype(np.float64)
+moments4 = [0] * num_moments
+print ("64:1 moments: 1st to 12th")
+for i in range(num_moments):
+  moments4[i] = stats.moment(b4, moment=i+1, axis=None )
+  print ("  %.4e" % moments4[i] )
+diff4 = [0.0] * num_moments
+for i in range(num_moments):
+	if moments1[i] != 0.0:
+		diff4[i] = (moments4[i]-moments1[i]) / moments1[i]
+	else:
+		diff4[i] = 0.0
+
+
+x = [i+1 for i in range(num_moments) ]
+
+plt.plot( x, diff2, 'r-', label="16:1" )
+plt.plot( x, diff3, 'g-', label="32:1" )
+plt.plot( x, diff4, 'b-', label="64:1" )
+
+plt.title('Difference in moments of vx ')
+plt.xlabel('Order of moments')
+plt.ylabel('Difference')
+plt.legend()
+# plt.show()
+plt.savefig('foo.pdf', bbox_inches='tight')
+
+
