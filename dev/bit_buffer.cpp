5ea1f8299681240ea0a60e06538e4a7964966f30
Samuel Li <shaomenglee@gmail.com>
2018-06-22 00:18:58 -0600

    improvement on format and functionality of bit_buffer class

--- dev/bit_buffer.cpp
+++ dev/bit_buffer.cpp
@@ -7,34 +7,29 @@
 //
 BitBuffer::BitBuffer( std::string name )
 {
-  fileName          = name;
-  buffer            = NULL;
-  Reset();
+    fileName          = name;
+    buffer            = NULL;
+    Reset();
 }
 
 BitBuffer::~BitBuffer()
 {
-  fileName.clear();
-  Reset();
+    fileName.clear();
+    Reset();
 }
 
 void BitBuffer::Reset()
 {
-  numOfFloats       = 0;
-  numOfBits         = 0;
-  currentByteIdx    = 0;
-  currentByte       = 0;
-  bitsToGo          = 0;
-  if( buffer )
-  {
-    delete[] buffer;
-    buffer  = NULL;
-  }
-}
-
-void BitBuffer::SetNumOfFloats( Int64 num )
-{
-  numOfFloats = num;
+    headerSize        = 0;  // Once header is specified, this is header's size.
+    numOfBits         = 0;
+    currentByteIdx    = 0;
+    currentByte       = 0;
+    bitsToGo          = 0;
+    if( buffer )
+    {
+      delete[] buffer;
+      buffer  = NULL;
+    }
 }
 
 
@@ -43,79 +38,77 @@ void BitBuffer::SetNumOfFloats( Int64 num )
 //
 bool InputBitBuffer::Start()
 {
-  FILE* filePtr = fopen( fileName.c_str(), "r" );
-  if( filePtr == NULL )
-  {
-    std::cerr << "Failed to open file: " << fileName << std::endl;
-    return false;
-  }
-  fseek( filePtr, 0, SEEK_END );
-  Int64 size = ftell( filePtr );
-  rewind( filePtr );
+    FILE* filePtr = std::fopen( fileName.c_str(), "r" );
+    if( filePtr == NULL )
+    {
+        std::cerr << "Failed to open file: " << fileName << std::endl;
+        return false;
+    }
+    std::fseek( filePtr, 0, SEEK_END );
+    Int64 size = std::ftell( filePtr );
+    std::rewind( filePtr );
 
-  try
-  {
-    buffer = new unsigned char[ size ];
-  }
-  catch (std::bad_alloc& ba)
-  {
-    std::cerr << "bad_alloc caught when reading file: " << ba.what() << std::endl;
-    return false;
-  }
+    assert( buffer == NULL );
+    try
+    {
+        buffer = new unsigned char[ size ];
+    }
+    catch (std::bad_alloc& ba)
+    {
+      std::cerr << "bad_alloc caught when reading file: " << ba.what() << std::endl;
+      return false;
+    }
 
-  fread( buffer, sizeof(unsigned char), size, filePtr );
-  fclose( filePtr );
-  
-  return true;
+    std::fread( buffer, sizeof(unsigned char), size, filePtr );
+    std::fclose( filePtr );
+    
+    return true;
 }
 
 bool InputBitBuffer::End()
 {
-  Reset();
-  return true;
+    Reset();
+    return true;
 }
 
-bool InputBitBuffer::GetHeader( Float64* header )
-{
-  memcpy( header, buffer, sizeof(Float64) * numOfFloats );
-  currentByteIdx += sizeof(Float64) * numOfFloats;
-
-  return true;
-}
 
 bool InputBitBuffer::GetBit( Int32* bitValue )
 {
-  if( bitsToGo == 0 )
-  {
-    currentByte = buffer[ currentByteIdx++ ];
-    bitsToGo = 8;
-  }
+    if( bitsToGo == 0 )
+    {
+      currentByte = buffer[ currentByteIdx++ ];
+      bitsToGo = 8;
+    }
 
-  *bitValue = currentByte & 0x01;
-  currentByte >>= 1;
-  bitsToGo--;
+    *bitValue = currentByte & 0x01;
+    currentByte >>= 1;
+    bitsToGo--;
 
-  return true;
+    return true;
 }
 
 
 //
 // OutputBitBuffer
 //
+void OutputBitBuffer::SetNumberOfBits( Int64 num )
+{
+    numOfBits = num;
+}
+
 bool OutputBitBuffer::Start()
 {
-  Int64 totalSize = numOfFloats * 8;
-  totalSize      += numOfBits / 8;
+  Int64 totalSize = headerSize + numOfBits / 8;
   if( numOfBits % 8 > 0 )
-    totalSize++;
+      totalSize++;
   try
   {
-    buffer = new unsigned char[ totalSize ];
+      buffer = new unsigned char[ totalSize ];
   }
   catch (std::bad_alloc& ba)
   {
-    std::cerr << "bad_alloc caught when allocating output buffer: " << ba.what() << std::endl;
-    return false;
+      std::cerr << "bad_alloc caught when allocating output buffer: " << ba.what() << std::endl;
+      return false;
   }
 
   return true;
@@ -124,22 +117,21 @@ bool OutputBitBuffer::Start()
 
 bool OutputBitBuffer::End()
 {
-  Int64 totalSize = numOfFloats * 8;
-  totalSize      += numOfBits / 8;
+  Int64 totalSize = headerSize + numOfBits / 8;
   if( numOfBits % 8 > 0 )
   {
-    totalSize++;
-    // do something to pad the last few bits
+      totalSize++;
+      // do something to pad the last few bits
   }
 
-  FILE* f = fopen( fileName.c_str(), "w" );
+  FILE* f = std::fopen( fileName.c_str(), "w" );
   if( f == NULL )
   {
-    std::cerr << "Failed to open file: " << fileName << std::endl;
-    return false;
+      std::cerr << "Failed to open file: " << fileName << std::endl;
+      return false;
   }
-  fwrite( buffer, sizeof(unsigned char), totalSize, f );
-  fclose( f ); 
+  std::fwrite( buffer, sizeof(unsigned char), totalSize, f );
+  std::fclose( f ); 
 
   Reset();
 
--- dev/bit_buffer.h
+++ dev/bit_buffer.h
@@ -4,22 +4,23 @@ using namespace wavelet;
 class BitBuffer
 {
 public:
-  BitBuffer( std::string name);       // constructor, specify file name
- ~BitBuffer();                        // Destructor
-  void SetNumOfFloats( Int64 num );   // This is for the header
-  void Reset();
+    BitBuffer( std::string name);       // constructor, specify file name
+   ~BitBuffer();                        // Destructor
+    void Reset();
 
-  virtual bool Start()    = 0;
-  virtual bool End()      = 0;
+    // Each Start() call needs to be followed by an End(),
+    //   before the next Start() call.
+    virtual bool Start()    = 0;
+    virtual bool End()      = 0;
 
 protected:
-  std::string        fileName;
-  Int64              numOfFloats;        // total number of 64-bit floats as header
-  Int64              numOfBits;          // total number of bits
-  Int64              currentByteIdx;     // index of the current byte
-  unsigned char*     buffer;
-  unsigned char      currentByte;        // stores current char;
-  Int32              bitsToGo;           // how many bits untill finish currentByte
+    std::string        fileName;
+    Int64              headerSize;         // header size in byte
+    Int64              numOfBits;          // total number of bits
+    Int64              currentByteIdx;     // index of the current byte
+    unsigned char*     buffer;
+    unsigned char      currentByte;        // stores current byte;
+    Int32              bitsToGo;           // how many bits untill finish currentByte
 };
 
 
@@ -29,11 +30,10 @@ protected:
 class InputBitBuffer : BitBuffer
 {
 public:
-  bool Start();
-  bool End();
+    bool Start();
+    bool End();
 
-  bool GetBit( Int32* bitValue );     // zero means 0, non-zero means 1
-  bool GetHeader( Float64* header );  // reads the header: numOfFloats 64-bit floats
+    bool GetBit( Int32* bitValue );     // zero means 0, non-zero means 1
 };
 
 
@@ -43,10 +43,9 @@ public:
 class OutputBitBuffer : BitBuffer
 {
 public:
-  bool Start();
-  bool End();
-  void SetNumberOfBits( Int64 num );
+    bool Start();
+    bool End();
+    void SetNumberOfBits( Int64 num );
 
-  bool PutBit( Int32 bitValue );      // zero means 0, non-zero means 1
-  bool PutHeader( Float64* header );  // puts the header: numOfFloats 64-bit floats
+    bool PutBit( Int32 bitValue );      // zero means 0, non-zero means 1
 };
