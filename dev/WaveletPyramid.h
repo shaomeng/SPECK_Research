ea100c3bf648604c67373f8722a039262100a03b
Samuel Li <shaomeng@ucar.edu>
2018-08-17 16:22:18 -0600

    start to implement wavelet pyramid and blocks

---
+++ dev/WaveletPyramid.h
@@ -0,0 +1,19 @@
+#include "Wavelets.h"
+
+using namespace SPECK;
+
+
+// ----------------------------------------------------------------------------
+// WaveletPyramid class provides better APIs to organize wavelet coefficients
+//   into pyramid of blocks.
+// ----------------------------------------------------------------------------
+class WaveletPyramid
+{
+
+};
+
+
+class Set
+{
+
+};
