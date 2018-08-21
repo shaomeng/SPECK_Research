445a1f0786758bb9d0221dbff3819e7f57de9830
Samuel Li <shaomeng@ucar.edu>
2018-08-20 23:52:47 -0600

    fixing compiler errors for WaveletPyramid

--- dev/WaveletPyramid.cpp
+++ dev/WaveletPyramid.cpp
@@ -3,6 +3,72 @@
 #include <cstring>
 
 
+// ----------------------------------------------------------------------------
+// Class WaveletPyramid
+// ----------------------------------------------------------------------------
+
+// Constructor
+template<typename T>
+WaveletPyramid<T>::WaveletPyramid( Int32 nc, Int32 nr, Int32 nf, Int32 nlxy, Int32 nlz )
+    : numCols( nc ), numRows( nr ), numFrames( nf ), numLevelsXY( nlxy ), numLevelsZ( nlz )
+{
+    coeffBuffer = nullptr;
+}
+
+template<typename T>
+const T* WaveletPyramid::GetCoeffBuffer() const
+{
+    return coeffBuffer;
+}
+
+bool WaveletPyramid::HandOverBuffer( T* buf, Int64 numVals )
+{
+    if( numVals != (Int64)numCols * numRows * numFrames )
+        return false;
+
+    if( coeffBuffer )
+        delete[] coeffBuffer;
+    coeffBuffer = buf;
+
+    return true;
+}
+
+bool WaveletPyramid::HandOverBuffer( const float* buf, Int64 numVals )
+{
+    if( numVals != (Int64)numCols * numRows * numFrames )
+        return false;
+    
+    if( coeffBuffer )
+        delete[] coeffBuffer;
+
+    coeffBuffer = new (std::nothrow) T[ numVals ];
+    if( !coeffBuffer )
+        return false;
+
+    for( Int64 i = 0; i < numVals; i++ )
+        coeffBuffer[i] = (T)buf[i];
+
+    return true;
+}
+
+bool WaveletPyramid::HandOverBuffer( const double* buf, Int64 numVals )
+{
+    if( numVals != (Int64)numCols * numRows * numFrames )
+        return false;
+    
+    if( coeffBuffer )
+        delete[] coeffBuffer;
+
+    coeffBuffer = new (std::nothrow) T[ numVals ];
+    if( !coeffBuffer )
+        return false;
+
+    for( Int64 i = 0; i < numVals; i++ )
+        coeffBuffer[i] = (T)buf[i];
+
+    return true;
+}
+
 
 
 
@@ -11,16 +77,37 @@
 // ----------------------------------------------------------------------------
 // Class Set 
 // ----------------------------------------------------------------------------
+
+// Constructor
+Set::Set( const WaveletPyramid* P, Int32 sx, Int32 dx, Int32 sy, Int32 dy, Int32 sz, Int32 dz) 
+   : pyramid( p ), startX( sx ), dimX( dx ), startY( sy ), dimY( dy ), startZ( sz ), dimZ( dz )
+{}
+
 bool Set::IsSignificant( Int32 n )
 {
-    T   threshold = std::pow( 2.0, double(n) );
-    T*  buf = new T[ dimX ];
+    T   threshold   = std::pow( 2.0, double(n) );
+    T*  buf         = new T[ dimX ];
+    const T* coeffs = pyramid->GetCoeffBuffer();
     for( Int32 z = startZ; z < startZ + dimZ; z++ )
         for( Int32 y = startY; y < startY + dimY; y++ )
         {
-            std::memcpy( buf, 
+            std::memcpy( buf, coeffs + z * pyramid.numCols * pyramid.Rows +
+                                       y * pyramid.numCols + startX, sizeof(T) * dimX );
+            for( Int32 x = 0; x < dimX; x++ )
+                if( buf[x] > threshold )
+                {
+                    delete[] buf;
+                    return true;
+                }
         }
 
     delete[] buf;
     return false;
 }
+
+
+// Explicit Template Instantiation
+class template WaveletPyramid<float>;
+class template WaveletPyramid<double>;
+class template Set<float>;
+class template Set<double>;
--- dev/WaveletPyramid.h
+++ dev/WaveletPyramid.h
@@ -10,14 +10,32 @@ using namespace SPECK;
 template <typename T>
 class WaveletPyramid
 {
+public:
+    const Int32   numCols;        // X axis dimension 
+    const Int32   numRows;        // Y axis dimension 
+    const Int32   numFrames;      // Z axis dimension 
+    const Int32   numLevelsXY;    // XY plane wavelet decomposition levels
+    const Int32   numLevelsZ;     // Z direction wavelet decomposition levels
 
-private:
-    T*      coeffBuffer;
-    Int32   numCols;        // X axis dimension 
-    Int32   numRows;        // Y axis dimension 
-    Int32   numFrame;       // Z axis dimension 
-    Int32   numLevelsXY;    // XY plane wavelet decomposition levels
-    Int32   numLevelsZ;     // Z direction wavelet decomposition levels
+    // Constructor
+    WaveletPyramid( Int32 nc, Int32 nr, Int32 nf, Int32 nlxy, Int32 nlz ); 
+
+    // WaveletPyramid takes over the ownership of this buffer.
+    // Whoever owned buf previously should NOT touch this buffer again.
+    // Upon successful, returns true. Otherwise, returns false, and the 
+    //   old buffer content stays the same.
+    bool HandOverBuffer( T* buf, Int64 numVals );
+
+    // WaveletPyramid makes a copy of this buffer
+    // Upon successful, returns true. Otherwise, returns false, and the 
+    //   old buffer content may be corrupt.
+    bool CopyOverBuffer( const float*  buf, Int64 numVals );
+    bool CopyOverBuffer( const double* buf, Int64 numVals );
+
+    const   T*    GetCoeffBuffer() const;
+
+protected:
+    T*            coeffBuffer;
 };
 
 
@@ -28,11 +46,17 @@ template <typename T>
 class Set
 {
 public:
-    bool    IsSignificant( Int32 n );     // Is this set significant wrt n
+    const Int32   startX, dimX;
+    const Int32   startY, dimY;
+    const Int32   startZ, dimZ;
+    
+    // Constructor
+    Set( const WaveletPyramid<T>* p,   Int32  sx, Int32 dx, 
+                                       Int32  sy, Int32 dy,
+                                       Int32  sz, Int32 dz );
+
+    bool    IsSignificant( Int32 n ) const;     // Is this set significant wrt n
 
 private:
-    const   WaveletPyramid*   pyramid;    // The wavelet pyramid that this Set belongs to
-    Int32   startX, dimX;
-    Int32   startY, dimY;
-    Int32   startZ, dimZ;
+    const   WaveletPyramid<T>*   pyramid;       // The wavelet pyramid that this Set belongs to
 };
